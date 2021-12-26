380dd33ed08cb1060f40f989f570735d2c359510
Maximilian Du <32785271+MaxDu17@users.noreply.github.com>
2021-12-26 18:56:16 -0500

    got figures

---
+++ LaTeX/academic/code.lol
@@ -0,0 +1,2 @@
+\contentsline {lstlisting}{\numberline {1}hello this is a test caption}{1}{}%
+\contentsline {lstlisting}{code.py}{1}{}%
---
+++ LaTeX/academic/code.py
@@ -0,0 +1,8 @@
+import numpy as np
+print("Hello world!")
+print("and another one")
+for j in range(10):
+    for i in range(20): #testing a comment out! 
+        print("hello I am in {i}, and {j}")
+
+numpy.asarray([1, 1, 1])
