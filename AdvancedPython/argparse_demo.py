0aac8c9dc15f92af86fde3b293bfd9c1e43eb9b3
MaxDu <maximilianjdu@gmail.com>
2022-07-07 21:29:05 -0700

    added tkinter

--- AdvancedPython/argparse_demo.py
+++ AdvancedPython/argparse_demo.py
@@ -10,8 +10,16 @@ parser.add_argument(
     help = "add help here"
 )
 
+parser.add_argument(
+    "--direct_flag",
+    action="store_true", # means that you store direct_flag = true if we use this flag
+    help = "add help here"
+)
+
 args = parser.parse_args()
 print(args.test)
+print(args.direct_flag)
 
-# run this as argparse_demo.py --test bob      -> should print out "bob"
-# run this as argparse_demo.py      -> should print out "clark"
+# run this as argparse_demo.py --test bob      -> should print out "bob" / False
+# run this as argparse_demo.py      -> should print out "clark" / False
+# run this as argparse_demo.py --test bob --direct_flag         -> should print out "bob" / True
