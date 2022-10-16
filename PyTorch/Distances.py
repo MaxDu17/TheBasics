5722be8362848b546164526139b9ea8d4b518862
Maximilian Du <32785271+MaxDu17@users.noreply.github.com>
2022-10-16 16:40:40 -0700

    Update Distances.py

--- PyTorch/Distances.py
+++ PyTorch/Distances.py
@@ -12,4 +12,4 @@ pairwise_l2 = torch.cdist(embeddings_1, embeddings_2, p = 2.0)
 print(pairwise_l2.shape) #should be B X P X R
 
 #NOT DONE
-#show cosine, dot product, etc 
+#show cosine, dot product, etc
